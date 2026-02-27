3e5d0ee1518c6ab2a3a881e7b0381fc77e9f4f25
grrbrhrrrurrghhha-cpu <grrbrhrrrurrghhha@gmail.com>
2026-02-27 17:16:18 +0000

    shorten code

--- nandnz/io.c
+++ nandnz/io.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-long int code[] = {-1, 2, 0, 2, -1, 1, 5, 8, 0};
-unsigned long int ip;
+long int code[] = {-1, 2, 0, 2, -1, 1, 5, 8, 0}, ip;
 
 int main(void)
 {
   while (code[ip + 2] >= 0)
   {
-    if (code[ip] >= 0 && code[ip + 1] >= 0 && code[ip + 2] >= 0)
+    if (code[ip] >= 0 && code[ip + 1] >= 0)
     {
       code[code[ip]] = ~(code[code[ip]] & code[code[ip + 1]]);
       if (code[code[ip]] != 0)
